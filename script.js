7e670061b0b2f41b85532d94f3a31fdc3e48e659
TTG3333 <65309965+TTG3333@users.noreply.github.com>
2023-01-19 09:08:33 -0500

    Added the option to choose your own verb list

--- script.js
+++ script.js
@@ -5,43 +5,50 @@ async function getData(url) {
     return response.json();
 }
 
-function chooseVerb(data) {
+function getVerbList(data) {
     var verbs = [];
     for (v in data) {
         verbs.push(v);
     }
-    var verb = verbs[Math.floor(Math.random() * verbs.length)];
-    return verb;
+    return verbs;
 }
 
-function handleData(verb) {
+function chooseRandomVerb(vList) {
+    var newVerb = vList[Math.floor(Math.random() * vList.length)];
+    while (vList.length > 1 && newVerb == verb) {
+        var newVerb = vList[Math.floor(Math.random() * vList.length)];
+    }
+    return newVerb;
+}
+
+function changeVerb(verb) {
     document.getElementById("verb").innerHTML = verb;
 }
 
 const prom = getData("./indicativo_preterito.json");
-var verbs, verb;
+var verbs, verbList, verb;
 prom.then(data => {
     verbs = data;
-    verb = chooseVerb(data);
-    handleData(verb);
+    verbList = getVerbList(data);
+    nextVerb();
 })
 
-function checkData(data, chosen) {
+function checkData() {
     for (f in forms) {
         f = forms[f];
-        if (document.getElementById(f).value.toLowerCase() == data[chosen][f].toLowerCase()) {
+        if (document.getElementById(f).value.toLowerCase() == verbs[verb][f].toLowerCase()) {
             document.getElementById(f + "_span").innerHTML = "¡Excelente!";
             document.getElementById(f + "_span").className = "correct";
         } else {
-            document.getElementById(f + "_span").innerHTML = data[chosen][f].toLowerCase();
+            document.getElementById(f + "_span").innerHTML = verbs[verb][f].toLowerCase();
             document.getElementById(f + "_span").className = "incorrect";
         }
     }
 }
 
 function nextVerb() {
-    verb = chooseVerb(verbs);
-    handleData(verb);
+    verb = chooseRandomVerb(verbList);
+    changeVerb(verb);
     clearAnswers();
 }
 
@@ -57,8 +64,28 @@ function findVerb() {
     var choice = document.getElementById("verbChoice").value.toLowerCase();
     if (choice in verbs) {
         verb = choice;
-        handleData(verb);
+        changeVerb(verb);
     }
     document.getElementById("verbChoice").value = "";
     clearAnswers();
+}
+
+function newVerbList() {
+    if (document.getElementById("verbList").value == "") {
+        return;
+    }
+    var listInput = document.getElementById("verbList").value.toLowerCase().split(" ");
+    var newList = [];
+    for (v in listInput) {
+        v = listInput[v];
+        if (v in verbs) {
+            newList.push(v);
+        }
+    }
+    document.getElementById("verbList").value = "";
+    if (newList.length == 0) {
+        return;
+    }
+    verbList = newList;
+    nextVerb();
 }
